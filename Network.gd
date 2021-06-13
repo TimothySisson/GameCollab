c2e8e500c8c023a78a043ded105708b64dc3f1cd
TimothySisson <sissonte@vcu.edu>
2021-06-13 18:04:43 -0400

    Added initial multiplayer functionality

---
+++ Network.gd
@@ -0,0 +1,37 @@
+extends Node
+
+const DEFAULT_PORT = 28960
+const MAX_CLIENTS = 5
+
+var server = null
+var client = null
+
+var ip_address = ""
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	ip_address = IP.get_local_addresses()[3]
+	
+	get_tree().connect("connected_to_server", self, "_connected_to_server")
+	get_tree().connect("server_disconnected", self, "_server_disconnected")
+	
+func _connected_to_server():
+	print("Successfully connected to server")
+	
+func _server_disconnected():
+	print("Disconnected from server")
+
+func create_server():
+	server = NetworkedMultiplayerENet.new()
+	server.create_server(DEFAULT_PORT, MAX_CLIENTS)
+	get_tree().set_network_peer(server)
+	
+func join_server():
+	client = NetworkedMultiplayerENet.new()
+	client.create_client(ip_address, DEFAULT_PORT)
+	get_tree().set_network_peer(client)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ NetworkSetup.gd
@@ -0,0 +1,37 @@
+extends Control
+
+
+onready var multiplayer_config_ui = $Config
+onready var server_ip_address = $Config/ServerIPAddress
+onready var device_ip_address = $CanvasLayer/DeviceIPAddress
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+# warning-ignore:return_value_discarded
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+# warning-ignore:return_value_discarded
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+	
+	device_ip_address.text = Network.ip_address
+	
+func _player_connected(id):
+	print("Player " + str(id) + " has connected")
+	
+func _player_disconnected(id):
+	print("Player " + str(id) + " has disconnected")
+
+func _on_CreateServer_pressed():
+	multiplayer_config_ui.hide()
+	Network.create_server()
+	print("Created server at " + Network.ip_address)
+	
+func _on_JoinServer_pressed():
+	if server_ip_address.text != "":
+		multiplayer_config_ui.hide()
+		Network.ip_address = server_ip_address.text
+		Network.join_server()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
