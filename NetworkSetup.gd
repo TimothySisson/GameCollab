9d9625421076d308d6bc06f1a2ed8cd81aefd2d9
TimothySisson <sissonte@vcu.edu>
2021-06-13 18:48:34 -0400

    Added game scene after joining server

--- NetworkSetup.gd
+++ NetworkSetup.gd
@@ -17,6 +17,7 @@ func _ready():
 func _player_connected(id):
 	print("Player " + str(id) + " has connected")
 	
+	
 func _player_disconnected(id):
 	print("Player " + str(id) + " has disconnected")
 
@@ -24,12 +25,17 @@ func _on_CreateServer_pressed():
 	multiplayer_config_ui.hide()
 	Network.create_server()
 	print("Created server at " + Network.ip_address)
+	var game = preload("res://Game.tscn").instance()
+	get_tree().get_root().add_child(game)
+
 	
 func _on_JoinServer_pressed():
 	if server_ip_address.text != "":
 		multiplayer_config_ui.hide()
 		Network.ip_address = server_ip_address.text
 		Network.join_server()
+		var game = preload("res://Game.tscn").instance()
+		get_tree().get_root().add_child(game)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
